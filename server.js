1fa6b6bd9bdf978d2ba01e7cbfa144c6d98a71a4
Steve <zjplove3618739792@gmail.com>
2024-05-31 15:16:07 -0400

    message describing code changes

--- server.js
+++ server.js
@@ -75,6 +75,33 @@ app.delete("/fruits/:fruitId", async (req, res) => {
   res.redirect("/fruits");
 });
 
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+
+// server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
